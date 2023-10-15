47dbca405b2c8c02431e4ba97e7f4975eb570819
xianren <2958197921@qq.com>
2023-10-15 20:51:38 +0800

    前缀和

---
+++ prefixSum/prefixSumArray/prefixSumArray.go
@@ -0,0 +1,22 @@
+package prefixSum
+
+type NumArray struct {
+	prefixSum []int
+}
+
+func Constructor(nums []int) NumArray {
+	// 创建一个前缀和数组
+	// 记录第零位前缀和为0,避免判断边界条件
+	prefixSum := make([]int, len(nums)+1)
+	sum := 0
+	for i, val := range nums {
+		prefixSum[i] = sum
+		sum += val
+	}
+	prefixSum[len(nums)] = sum
+	return NumArray{prefixSum: prefixSum}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	return this.prefixSum[right+1] - this.prefixSum[left]
+}
